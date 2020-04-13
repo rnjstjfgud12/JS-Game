fa3fbfbfc79de2a4d1e5a808a7eda224280c2624
rnjstjfgud12 <61874872+rnjstjfgud12@users.noreply.github.com>
2020-04-13 21:25:31 +0900

    Modify line
    line 4, 5의 값을 700으로 주어지면 이상한 좌표에서 라인이 그려져서 offset값으로 수정

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-const canvas = document.querySelector("jsCanvas");
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
+// 각각 700으로 값을 지정하니까 좌표가 다른 곳에서 그림이 그려져서 사용함
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y);
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event) {
+    painting = true;
 }
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
